c550ac898e4e6b29875f46e53f97ab1bd73180ee
Administrator <935340072@qq.com>
2018-10-21 23:38:57 +0800

    first

---
+++ .idea/q.js
@@ -0,0 +1,66 @@
+{
+    "code"
+:
+    0, "message"
+:
+    "ok", "result"
+:
+    [{
+        "productId": 400000003254015,
+        "productType": 40,
+        "productName": "JavaScript程序设计",
+        "imageUrl": "http://edu-image.nosdn.127.net/7ab02450-a5da-404d-9138-24394591bc34.jpg?imageView&quality=100",
+        "lessonName": "JSON ",
+        "lastLearnedTime": 1536586463684,
+        "refResId": null,
+        "lessonType": null,
+        "yoocCourseDto": null,
+        "s2LearnRecordDto": {"unitId": 400000004440165, "learnTextPage": null, "learnVideoTime": null}
+    }, {
+        "productId": 400000003266019,
+        "productType": 40,
+        "productName": "初级前端开发工程师",
+        "imageUrl": "http://edu-image.nosdn.127.net/7f21382b-ac05-423b-b66d-fd0100df4268.png?imageView&quality=100",
+        "lessonName": null,
+        "lastLearnedTime": 1536586463684,
+        "refResId": null,
+        "lessonType": null,
+        "yoocCourseDto": null,
+        "s2LearnRecordDto": {"unitId": null, "learnTextPage": null, "learnVideoTime": null}
+    }, {
+        "productId": 432008,
+        "productType": 0,
+        "productName": "8小时学会HTML网页开发",
+        "imageUrl": "http://img0.ph.126.net/BG6QytQBdnKkl8ubEaC9zw==/6599324865005014224.png",
+        "lessonName": null,
+        "lastLearnedTime": 1536032928136,
+        "refResId": null,
+        "lessonType": 2,
+        "yoocCourseDto": null,
+        "s2LearnRecordDto": null
+    }, {
+        "productId": 1004170004,
+        "productType": 0,
+        "productName": "web前端开发JavaScript精英课js",
+        "imageUrl": "http://edu-image.nosdn.127.net/b1e2a84e-6e6b-4b18-9a05-26ba1cc31149.jpg?imageView&quality=100&crop=0_0_800_449",
+        "lessonName": null,
+        "lastLearnedTime": 1535822018468,
+        "refResId": null,
+        "lessonType": null,
+        "yoocCourseDto": null,
+        "s2LearnRecordDto": null
+    }, {
+        "productId": 400000003264015,
+        "productType": 40,
+        "productName": "CSS",
+        "imageUrl": "http://edu-image.nosdn.127.net/ba3c52f3-0dfa-4e65-a2c7-eaabf5bedf1d.jpg?imageView&quality=100",
+        "lessonName": null,
+        "lastLearnedTime": 1535711772816,
+        "refResId": null,
+        "lessonType": null,
+        "yoocCourseDto": null,
+        "s2LearnRecordDto": {"unitId": null, "learnTextPage": null, "learnVideoTime": null}
+    }], "uuid"
+:
+    null
+}
