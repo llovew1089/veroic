4ab8cb6975f50ff351d0ef69f8ec0209db50b578
Administrator <935340072@qq.com>
2018-10-22 01:15:53 +0800

    first

--- DOM计分作业_大油饼/test.js
+++ DOM计分作业_大油饼/test.js
@@ -4,8 +4,10 @@
 var db = new DBComment();
 var user = db.getLoginUser();
 var id = +new Date;
+//当前登录用户头像
 var url = user.avatarURL;
 document.getElementById('user').src = url;
+//正确显示评论总数量
 db.getCommentTotal().then(function (total) {
     var total = '共' + JSON.stringify(total) + '条评论';
     var a = document.querySelector('.m-header .count ');
