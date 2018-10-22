1c42443c806ebb1c44d96c4aec44a8d6a7812afa
Administrator <935340072@qq.com>
2018-10-23 00:26:25 +0800

    first

--- DOM计分作业_大油饼/test.js
+++ DOM计分作业_大油饼/test.js
@@ -4,7 +4,7 @@
 var db = new DBComment();
 var user = db.getLoginUser();
 var id = +new Date;
-var pages=0;
+var pages = 0;
 //当前登录用户头像
 var url = user.avatarURL;
 document.getElementById('user').src = url;
@@ -12,11 +12,11 @@ document.getElementById('user').src = url;
 db.getCommentTotal(pages).then(function (total) {
     var tot = '共' + JSON.stringify(total) + '条评论';
     var count = document.querySelector('.m-header .count ');
-    count.textContent=tot;
-     pages=total;
+    count.textContent = tot;
+    pages = total;
     return pages;
 });
-var current=1;
+var current = 1;
 function limit(pages, current) {
     if (current <= (parseInt(pages / 10))) {
         return 10;
@@ -24,59 +24,102 @@ function limit(pages, current) {
         return pages % 10
     }
 }
-var limit=limit(pages,current);
+var limit = limit(pages, current);
 var opt = {
     // 当前页码
     page: current,
     // 每页显示数量
     limit: 10
 };
-db.getCommentList(opt).then(function(ret){
+db.getCommentList(opt).then(function (ret) {
     // ret - 数组，当前页评论列表
+    var copy = [];
+    for (var i = 0; i < ret.length; i++) {
+        copy[i] = ret[i]
+    }
+    console.log(copy);
+    for (var i = 0; i < opt.limit; i++) {
 
-    var li=document.createElement('li');
-    li.className='m-comment f-clear';
-    document.getElementById("myList").appendChild(li);
+        var li = document.createElement('li');
+        li.className = 'm-comment f-clear';
+        document.getElementById("myList").appendChild(li);
 
-    var a=document.createElement('a');
-    a.className='w-avatar';
-    a.href='#';
-    li.appendChild(a);
-    var img=document.createElement('img');
-    img.src="img/avatar4.png";
-    img.alt="大油饼";
-    a.appendChild(img);
-    var div1=document.createElement('div');
-    div1.className='reply';
-    li.appendChild(div1);
-    var p=document.createElement('p');
-    div1.appendChild(p);
-    var time=document.createElement('time');
-    time.textContent="10月10日";
-    p.appendChild(time);
-    var p1=document.createElement('p');
-    div1.appendChild(p1);
-    var a1=document.createElement('a');
-    a1.href='#';
-    a1.textContent='删除';
-    p1.appendChild(a1);
-    var div2=document.createElement('div');
-    div2.className='comment';
-    li.appendChild(div2);
-    var p2=document.createElement('p');
-    p2.textContent='好听哦真死牛逼是了'; div2.appendChild(p2);
-    var a2=document.createElement('a');
-    a2.className='user';
-    a2.href='#';
-    a2.textContent='大油饼'; p2.appendChild(a2);
+        var a = document.createElement('a');
+        a.className = 'w-avatar';
+        a.href = '#';
+        li.appendChild(a);
+        var img = document.createElement('img');
+        img.src = copy[i].user.avatarURL;
+        img.alt = copy[i].user.id;
+        a.appendChild(img);
+        var div1 = document.createElement('div');
+        div1.className = 'reply';
+        li.appendChild(div1);
+        var p = document.createElement('p');
+        div1.appendChild(p);
+        var time = document.createElement('time');
+        time.textContent =displayDate(copy[i].time);
+        p.appendChild(time);
+        var p1 = document.createElement('p');
+        div1.appendChild(p1);
+        var a1 = document.createElement('a');
+        a1.href = '#';
+        a1.textContent = '删除';
+        p1.appendChild(a1);
+        var div2 = document.createElement('div');
+        div2.className = 'comment';
+        li.appendChild(div2);
+        var p2 = document.createElement('p');
+        //p2.textContent = ':'+copy[i].content;
+        p2.textContent = ':buydsabufa';
+        div2.appendChild(p2);
+        var a2 = document.createElement('a');
+        a2.className = 'user';
+        a2.href = '#';
+        a2.textContent = copy[i].user.nickName;
+        p2.appendChild(a2);
+    }
 
-
-
-
-
-
-
-
-
+    function displayDate(time) {
+        var now = new Date();
+        var oldDate = new Date(time);
+        var oldyear = oldDate.getFullYear();
+        var oldmonth = oldDate.getMonth() + 1;    //js从0开始取
+        var oldday = oldDate.getDate();
+        var oldhour = oldDate.getHours();
+        var oldminutes = oldDate.getMinutes();
+        var oldsecond = oldDate.getSeconds();
+        var oldminutesText;
+        oldminutesText = oldminutes < 10 ? "0" + oldminutes : oldminutes;
+        var disPlayTime;
+        //判断是否为今年
+        if (now.getFullYear() != oldyear) {
+            disPlayTime = oldyear + "-" + oldmonth + "-" + oldday + " " + oldhour + ":" + oldminutesText;
+            return disPlayTime;
+        }
+        //判断是否为今日
+        else if ((now.getFullYear() == oldyear)&&(now.getDate() != oldday)) {
+            disPlayTime =oldmonth + "月" + oldday + "日";
+            return disPlayTime;
+        }
+        else {
+            //判断小时是否一致
+            if (now.getHours() != oldhour) {
+                disPlayTime = oldhour + ":" + oldminutesText;
+            }
+            else {
+                //判断分钟是否一致
+                if (now.getMinutes() != oldminutes) {
+                    disPlayTime = Math.abs(oldminutes - now.getMinutes()) + " 分前";
+                }
+                else {
+                    disPlayTime = Math.abs(oldsecond - now.getSeconds()) + " 秒前";
+                }
+            }
+        }
+        return disPlayTime;
+    }
 });
+
+
 
